f4f070e1780691d7b5384271e8c9854a7e62bfde
Kishore D Rajegowda <kishore328@gmail.com>
2021-05-24 23:15:01 +0530

    sytles updated

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ class App extends Component {
         return (
             <div>
                 <div className="calculator-body">
-                    <h1>Simple Calculator</h1>
+                    <h1 className="">Simple Calculator</h1>
                     <ResultComponent result={this.state.result}/>
                     <KeyPadComponent onClick={this.onClick}/>
                 </div>
